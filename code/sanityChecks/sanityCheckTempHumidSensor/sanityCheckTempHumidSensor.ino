53fcb317aaf34e23f24ff27292e311a00d7c4e20
Alexandru Cohal <alexandru.cohal@yahoo.com>
2021-04-05 21:16:17 +0200

    Sanity check for temperature and humidity sensor DHT11 added

---
+++ code/sanityChecks/sanityCheckTempHumidSensor/sanityCheckTempHumidSensor.ino
@@ -0,0 +1,32 @@
+#include "DHT.h"
+
+#define DHTPIN 8
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() 
+{
+  Serial.begin(9600);  
+  dht.begin();
+}
+
+void loop() 
+{
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  float hindex = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" deg C; ");
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %");
+  Serial.print("Heat index: ");
+  Serial.print(hindex);
+  Serial.print(" deg C; ");
+  Serial.println("");
+
+  delay(1000);
+}
